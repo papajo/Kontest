d381221bb1e3b2e4f080f2e2dd036920de645bbd
papajo <joshipv2@gmail.com>
2018-09-25 10:54:43 -0400

    get newBlock

--- jsblockchain.js
+++ jsblockchain.js
@@ -25,4 +25,14 @@ class BlockChain {
     createGenesisBlock(){
         return new Block(0,"09/24/2018","genesis block","0");
     }
+
+    getLatestBlock(){
+        return this.chain[this.chain.length - 1];
+    }
+
+    addBlock(newBlock){
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock);
+    }
 }
