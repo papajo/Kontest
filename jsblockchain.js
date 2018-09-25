4c123d51eaed9a76d2aca9d24b1a5a5d8b115a14
papajo <joshipv2@gmail.com>
2018-09-25 12:57:09 -0400

    check if block chain is valid

--- jsblockchain.js
+++ jsblockchain.js
@@ -35,4 +35,19 @@ class BlockChain {
         newBlock.hash = newBlock.calculateHash();
         this.chain.push(newBlock);
     }
+
+    checkBlockChainIsValid(){
+        for(let i=1; i < this.chain.length; i++){
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i-1];
+
+            if(currentBlock.hash !== currentBlock.calculateHash()) {
+                return false;
+            }
+            if(currentBlock.previousHash !== previousBlock.hash) {
+                return false;
+            }
+            return true;
+        }
+    }
 }
