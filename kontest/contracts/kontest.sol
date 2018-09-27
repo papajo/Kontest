2181416b3ec02251bf487b628fabb16321dcc766
papajo <joshipv2@gmail.com>
2018-09-27 15:34:06 -0400

    struct and addKontestant

--- kontest/contracts/kontest.sol
+++ kontest/contracts/kontest.sol
@@ -3,13 +3,25 @@ pragma solidity ^0.4.24;
 //creating the contract
 contract Kontest {
 
-	//declare the variables
-	string public kontestant;
-	
-	//constructor
-	function Kontest() public {
+	//create a structure modeling the kontestant
+	struct Kontestant {
+		uint id;
+		string name;
+		uint voteCount;
+	}
+	//mapping to fetch the kontestant details
+	mapping(uint => Kontestant) public kontestants;
+	//keep track of kontestants count 
+	uint public kontestantsCount;
 
-		kontestant = "Tom";
+	function Kontest () public {
+		addKontenstant("Tom");
+		addKontenstant("Jerry");
 	}
 
+	//add a kontestant
+	function addKontenstant(string _name) private {
+		kontestantsCount++;
+		kontestants[kontestantsCount] = Kontestant(kontestantsCount, _name, 0);
+	}
 }
