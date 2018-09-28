006948d25064216b2d2f4a3fda4bf853ddd86294
papajo <joshipv2@gmail.com>
2018-09-27 20:39:33 -0400

    finish with listing kontestant accounts

--- kontest/src/js/app.js
+++ kontest/src/js/app.js
@@ -10,39 +10,42 @@ App = {
   initWeb3: function() {
     if(typeof web3 !== 'undefined') {
       //i.e. if a web3 instance is running in the browser via metamask
-      App.web3Provider web3.currentProvider;
+      App.web3Provider = web3.currentProvider;
       web3 = new Web3(web3.currentProvider);
     } else {
       //if no web3 default instance then provide one
       App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
       web3 = new Web3(web3.web3Provider);
     }
-  }
+
     return App.initContract();
   },
 
+  
   initContract: function() {
     $.getJSON("Kontest.json", function(kontest) {
-      //Instantiate a new truffle contract from the artifact
+      // Instantiate a new truffle contract from the artifact
       App.contracts.Kontest = TruffleContract(kontest);
-      //Connect provider to interact with contract
-      App.Contracts.Kontest.setProvider(App.web3Provider);
-    });
+      // Connect provider to interact with contract
+      App.contracts.Kontest.setProvider(App.web3Provider);
+
+    //App.listenForEvents();  
     return App.render();
+    });
   },
 
   render: function() {
     var kontestInstance;
-    var loader = "#loader";
-    var content = "content";
+    var loader = $("#loader");
+    var content = $("#content");
 
     loader.show();
     content.hide();
 
     //Load account data
     web3.eth.getCoinbase(function(err, account) {
-      if (err == null) {
-        App.account - account;
+      if (err === null) {
+        App.account = account;
         $("#accountAddress").html("Your Account: " + account);
       }
     });
@@ -51,17 +54,26 @@ App = {
       kontestInstance = instance;
       return kontestInstance.kontestantsCount();
     }).then(function(kontestantsCount) {
-      var kontestantsResults = $("#kontestantsResults");
+      var kontestantsResults = $("#contestantsResults");
       kontestantsResults.empty();
+
+      //var kontestantsSelect = $('#contestantsSelect');
+      //kontestantsSelect.empty();
 
       for (var i = 1; i <= kontestantsCount; i++) {
         kontestInstance.kontestants(i).then(function(kontestant) {
           var id = kontestant[0];
           var name = kontestant[1];
           var voteCount = kontestant[2];
-          //Render Kontestant Result
+
+          // Render contestant Result
           var kontestantTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + voteCount + "</td></tr>"
           kontestantsResults.append(kontestantTemplate);
+
+          // Render candidate voting option
+          //var kontestantOption = "<option value='" + id + "' >" + name + "</ option>"
+          //kontestantsSelect.append(kontestantOption);
+
         });
       }
       loader.hide();
@@ -69,10 +81,8 @@ App = {
     }).catch(function(error) {
       console.warn(error);
     });
-  }  
-
-};
-
+  },
+}
 $(function() {
   $(window).load(function() {
     App.init();
