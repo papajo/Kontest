2a39b7ee2a23a2921445ae4714deacbfd5b95c81
papajo <joshipv2@gmail.com>
2018-09-28 09:05:27 -0400

    build deploy and test voting mechanism

--- kontest/contracts/kontest.sol
+++ kontest/contracts/kontest.sol
@@ -11,6 +11,10 @@ contract Kontest {
 	}
 	//mapping to fetch the kontestant details
 	mapping(uint => Kontestant) public kontestants;
+
+	//keep a list of users who cast the vote
+	mapping(address => bool) public voters;
+
 	//keep track of kontestants count 
 	uint public kontestantsCount;
 
@@ -18,10 +22,21 @@ contract Kontest {
 		addKontenstant("Tom");
 		addKontenstant("Jerry");
 	}
-
 	//add a kontestant
 	function addKontenstant(string _name) private {
 		kontestantsCount++;
 		kontestants[kontestantsCount] = Kontestant(kontestantsCount, _name, 0);
+	}
+	//prevent from duplicate voting
+	function vote (uint _kontestantId) public {
+
+		require (!voters[msg.sender]);
+		require(_kontestantId > 0 && _kontestantId <= kontestantsCount);
+
+		//increment the voters vote count 
+		kontestants[_kontestantId].voteCount++ ;
+		//set the voters voted status to true
+		voters[msg.sender] = true;
+
 	}
 }
--- kontest/test/kontest.js
+++ kontest/test/kontest.js
@@ -10,4 +10,38 @@ contract("Kontest", function(accounts) {
 			assert.equal(count, 2);
 		});
 	});
+
+	it("it initializes the contestants with the correct values", function() {
+    return Kontest.deployed().then(function(instance) {
+      kontestInstance = instance;
+      return kontestInstance.kontestants(1);
+    }).then(function(kontestant) {
+      assert.equal(kontestant[0], 1, "contains the correct id");
+      assert.equal(kontestant[1], "Tom", "contains the correct name");
+      assert.equal(kontestant[2], 0, "contains the correct votes count");
+      return kontestInstance.kontestants(2);
+    }).then(function(kontestant) {
+      assert.equal(kontestant[0], 2, "contains the correct id");
+      assert.equal(kontestant[1], "Jerry", "contains the correct name");
+      assert.equal(kontestant[2], 0, "contains the correct votes count");
+    });
+  });
+
+	it("allows a voter to cast a vote", function() {
+    return Kontest.deployed().then(function(instance) {
+      kontestInstance = instance;
+      kontestantId = 2;
+      return kontestInstance.vote(kontestantId, { from: accounts[0] });
+    }).then(function(receipt) {
+      return kontestInstance.voters(accounts[0]);
+    }).then(function(voted) {
+      assert(voted, "the voter was marked as voted");
+      return kontestInstance.kontestants(kontestantId);
+    }).then(function(kontestant) {
+      var voteCount = kontestant[2];
+      assert.equal(voteCount, 1, "increments the kontestant's vote count");
+    })
+  });
+
+
 });
